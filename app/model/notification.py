94467afca95d7e24e8a48d87667defb417250e32
Doron Ben Elazar <Doron.BenElazar@microsoft.com>
2018-07-24 14:05:10 +0300

    Scaffolding notification model

---
+++ app/model/notification.py
@@ -0,0 +1,16 @@
+from pymongo.collection import Collection, ObjectId
+
+
+class Notification(object):
+    def __init__(self):
+        super(Notification, self).__init__()
+
+    @staticmethod
+    def find(face_collection, object_id):
+        """
+
+        :type face_collection: Collection
+        :type object_id: ObjectId
+        :return:
+        """
+        pass
